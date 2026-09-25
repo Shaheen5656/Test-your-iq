da1f9bf516a8040b26bf44eb4b3aa03b4676e5b6
Shaheen5656 <hblacounting2@gmail.com>
2026-09-25 13:34:09 +0500

    Fix InterstitialAd import for Google Mobile Ads SDK

--- app/src/main/java/com/mindspark/iq/MainActivity.java
+++ app/src/main/java/com/mindspark/iq/MainActivity.java
@@ -11,9 +11,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
 import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
